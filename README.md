6b4a13d2b168464b94b8b587610c21333df8c58a
Seohyun Lee <studioseorii@gmail.com>
2022-01-15 12:52:57 +0900

    Sync

--- README.md
+++ README.md
@@ -3,26 +3,6 @@
 ```powershell
 Set-ExecutionPolicy Unrestricted
 
-$scriptUrl = "https://raw.githubusercontent.com/HancomAC/InitCom/main/run.ps1"
-$destination = "c:\test.txt"
-$scriptName = "c:\test.ps1"
-
-Invoke-WebRequest $scriptUrl -OutFile $destination
-
-# if the file was downloaded, delete the old script file and rename the new
-# file
-if(test-path $destination){
-    remove-item $scriptName
-    Rename-Item $destination $scriptName
-}
-
-&$scriptName
-```
-
-
-```powershell
-Set-ExecutionPolicy Unrestricted
-
 $scriptUrl = "https://raw.githubusercontent.com/HancomAC/InitCom/main/install.ps1"
 $destination = "c:\test.txt"
 $scriptName = "c:\test.ps1"
--- install.ps1
+++ install.ps1
@@ -1,11 +1,11 @@
 if (-not(Test-Path -Path "C:/JungolTemp")) {
 	    New-Item -Path "C:\" -Name "JungolTemp" -ItemType "directory"
-	
+
 	    Invoke-WebRequest -Uri "https://repo.anaconda.com/archive/Anaconda3-2021.11-Windows-x86_64.exe" -OutFile "C:/JungolTemp/anaconda.exe"
 	    Invoke-WebRequest -Uri "http://jungol.co.kr/data/codeblocks-20.03mingw-setup.exe" -OutFile "C:/JungolTemp/cb.exe"
 	    Invoke-WebRequest -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user" -OutFile "C:/JungolTemp/vscode.exe"
 	}
-	
-	Start-Process "C:/JungolTemp/cb.exe /s"
-	Start-Process "C:/JungolTemp/vscode.exe /Silent"
-	Start-Process "C:/JungolTemp/anaconda.exe /s"
+
+& "C:/JungolTemp/cb.exe /s"
+& "C:/JungolTemp/vscode.exe /Silent"
+& "C:/JungolTemp/anaconda.exe /s"
