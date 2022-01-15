065ed8ae9af286c791d003dc3dbcc385735d82be
Seohyun Lee <studioseorii@gmail.com>
2022-01-15 14:35:56 +0900

    Sync

--- final.ps1
+++ final.ps1
@@ -1,14 +1,22 @@
-New-Item -Path HKCU:\Software\Policies\Microsoft\Edge -Name RestoreOnStartupURLs –Force
-
+$registryPath = 'HKCU:\Software\Policies\Microsoft\Edge'
+$name = 'RestoreOnStartup'
+$value = 0x00000004
+IF(!(Test-Path $registryPath))
+  {
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+ ELSE {
+    New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null}
+New-Item -Path "HKCU:\Software\Policies\Microsoft\Edge" -Name RestoreOnStartupURLs –Force
 $path = 'HKCU:\Software\Policies\Microsoft\Edge\RestoreOnStartupURLs'
-
 $name = '1'
-
 $value = 'http://jungol.co.kr'
-
 Set-Itemproperty -Path $path -Name $name -Value $value
 
-New-Item -ItemType SymbolicLink -Path "C:\Users\student\Desktop" -Name "JUNGOL.lnk" -Value "http://jungol.co.kr"
+$Shell = New-Object -ComObject ("WScript.Shell")
+$Favorite = $Shell.CreateShortcut("C:\Users\student\Desktop\JUNGOL.lnk")
+$Favorite.TargetPath = "http://jungol.co.kr";
+$Favorite.Save()
 
 Invoke-WebRequest -Uri "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_windows-x64_bin.zip" -OutFile "C:/JungolTemp/java.zip"
 Invoke-WebRequest -Uri "https://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2021-12/R/eclipse-inst-jre-win64.exe" -OutFile "C:/JungolTemp/eclipse.zip"
